c94f4cbb2b988e685e2d4d1f47e4231911b77df5
Antonov Andrey <aantonov@tradingview.com>
2023-11-11 21:40:55 +0300

    Grid color

--- visualization/utils.js
+++ visualization/utils.js
@@ -5,7 +5,11 @@ import config from './config.json' assert { type: 'json' };
 import os from "os";
 
 async function createCanvas(values) {
-	const yourVlSpec = { mark: "line", encoding: { x: {field: 'frame', type: 'quantitative'}, y: {field: 'averageReward', type: 'quantitative'} } };
+	const yourVlSpec = {
+		mark: "line",
+		config: { axis: { grid: true,  gridColor: config.gridColor } },
+		encoding: { x: {field: 'frame', type: 'quantitative'}, y: {field: 'averageReward', type: 'quantitative'} }
+	};
 	const vegaspec = lite.compile({ ...yourVlSpec, data: { values } }).spec;
 	const view = new vega.View(vega.parse(vegaspec), {renderer: "none"});
 	return await view.toCanvas()
@@ -13,12 +17,12 @@ async function createCanvas(values) {
 
 let bot = null;
 export async function sendDataToTelegram(values, message) {
-	if (bot === null) {
+	if (bot === null && config.token.length > 0) {
 		bot = new TelegramBot(config.token, {polling: true});
 	}
 	console.log(`Send value to telegram ${message}`);
 	const canvas = await createCanvas(values);
 	const data = await canvas.toBuffer();
-	await bot.sendMessage(config.chart_id, message + ':' + os.hostname());
-	await bot.sendPhoto(config.chart_id, data);
+	await bot?.sendMessage(config.chart_id, message + ':' + os.hostname());
+	await bot?.sendPhoto(config.chart_id, data);
 }
