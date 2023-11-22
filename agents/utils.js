e67549f0b6ce36cf55334aea684388df13877274
p9gehka <p9gehka@mail.ru>
2023-11-22 05:22:22 +0300

    Correct tabs

--- agents/utils.js
+++ agents/utils.js
@@ -80,23 +80,23 @@ export class Orders {
 
 export function getStateTensor(state, h, w, channels) {
 	const c = channels.length;
-  const numExamples = state.length;
+	const numExamples = state.length;
 
-  let buffer = tf.buffer([numExamples, h, w, c]);
-  for (let n = 0; n < numExamples; ++n) {
-    if (state[n] === null) {
-      continue;
-    }
-    channels.forEach((channel, i) => {
-    	for (let entity in channel) {
-	   		if (state[n][entity] === undefined) {
-	   			return
-	   		}
-	   		const enitities = state[n][entity].forEach(yx => {
-	   		  buffer.set(channel[entity], n, yx[0], yx[1], i);
-	   		})
-    	}
-    });
-  }
-  return buffer.toTensor();
+	let buffer = tf.buffer([numExamples, h, w, c]);
+	for (let n = 0; n < numExamples; ++n) {
+		if (state[n] === null) {
+			continue;
+		}
+		channels.forEach((channel, i) => {
+			for (let entity in channel) {
+				if (state[n][entity] === undefined) {
+					return
+				}
+				const enitities = state[n][entity].forEach(yx => {
+					buffer.set(channel[entity], n, yx[0], yx[1], i);
+				})
+			}
+		});
+	}
+	return buffer.toTensor();
 }
