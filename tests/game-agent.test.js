b03af68375c04fe6ad1c553833487eca9ed64e7c
p9gehka <p9gehka@mail.ru>
2023-11-23 19:17:05 +0300

    Add mock model

--- tests/game-agent.test.js
+++ tests/game-agent.test.js
@@ -1,13 +1,18 @@
+import tf from '@tensorflow/tfjs-node';
 import { Warhammer } from '../environment/warhammer.js';
 import { PlayerEnvironment } from '../environment/player-environment.js';
 import { GameAgent } from '../agents/game-agent0.1.js';
 
 describe('game agent', () => {
-	it('order', () => {
+	it('order', async () => {
+		const nn = [];
+		nn[0] = await tf.loadLayersModel(`file://tests/mock/dqn-test/model.json`);
+		nn[1] = await tf.loadLayersModel(`file://tests/mock/dqn-test/model.json`);
+
 		for(let i = 0; i<30; i++) {
 			const env = new Warhammer();
 			const player = new PlayerEnvironment(0, env);
-			const gameAgent = new GameAgent(player);
+			const gameAgent = new GameAgent(player, { nn });
 			let action = null;
 			player.step = (order) => {
 				action = order.action;
