064df8ba345c9d6d93f7a327b6025b586f12312e
p9gehka <p9gehka@mail.ru>
2023-11-23 15:39:45 +0300

    Correct argmax mask

--- tests/game-agent.test.js
+++ tests/game-agent.test.js
@@ -3,14 +3,19 @@ import { PlayerEnvironment } from '../environment/player-environment.js';
 import { GameAgent } from '../agents/game-agent0.1.js';
 
 describe('game agent', () => {
-  it('order', () => {
-     const env = new Warhammer();
-     const player = new PlayerEnvironment(0, env);
-     const gameAgent = new GameAgent(player);
-     let action = null;
-     player.step = (order) => action = order.action;
+	it('order', () => {
+		for(let i = 0; i<30; i++) {
+			const env = new Warhammer();
+			const player = new PlayerEnvironment(0, env);
+			const gameAgent = new GameAgent(player);
+			let action = null;
+			player.step = (order) => {
+				action = order.action;
+				return [order, env.getState(), 0];
+			};
 
-     gameAgent.playStep();
-     expect(action).toMatch(/NEXT_PHASE|SELECT$/);
-  });
+			gameAgent.playStep();
+			expect(action).toMatch(/NEXT_PHASE|SELECT$/);
+		}
+	});
 });
