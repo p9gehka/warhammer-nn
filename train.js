9ce46e60e45cb1c13067b8cab2d4c081c918072a
p9gehka <p9gehka@mail.ru>
2024-01-05 23:12:43 +0300

    Test 1 object scoring

--- train.js
+++ train.js
@@ -44,7 +44,6 @@ const learningRate = 1e-3;
 const savePath = './models/dqn';
 const syncEveryFrames = 1e3;
 const cumulativeRewardThreshold = 42;
-const syncEveryFrames = 6e3;
 const sendMessageEveryFrames = 1e3;
 const rewardAverager100Len = 100;
 
