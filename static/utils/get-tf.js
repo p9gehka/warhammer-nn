63374d2e9d15d1fd11b58aa024e03cf1a3c76389
p9gehka <p9gehka@mail.ru>
2024-02-28 23:03:33 +0300

    Fix process import

--- static/utils/get-tf.js
+++ static/utils/get-tf.js
@@ -2,7 +2,7 @@ export async function getTF() {
 	if (typeof window !== 'undefined') {
 		return Promise.resolve(window.tf);
 	}
-	const process = import('node:process');
+	const process = await import('node:process');
 	if (process.argv?.includes('-gpu')) {
 		console.log("USE TFJS-GPU")
 		return import ('@tensorflow/tfjs-node-gpu');
