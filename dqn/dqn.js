a7447e67a706d0406c57a0c827a332d6ed862496
p9gehka <p9gehka@mail.ru>
2024-03-06 12:16:10 +0300

    Increase filters

--- dqn/dqn.js
+++ dqn/dqn.js
@@ -31,12 +31,12 @@ export function createDeepQNetwork(numActions, h, w, c) {
 	}
 
 	const model = tf.sequential();
-	model.add(tf.layers.conv2d({filters: 64, kernelSize: 8,strides: 2, activation: 'relu', inputShape: [h, w, c] }));
+	model.add(tf.layers.conv2d({filters: 128, kernelSize: 8,strides: 2, activation: 'relu', inputShape: [h, w, c] }));
 	model.add(tf.layers.batchNormalization());
-	model.add(tf.layers.conv2d({filters: 128, kernelSize: 4,strides: 1, activation: 'relu'}));
+	model.add(tf.layers.conv2d({filters: 256, kernelSize: 4,strides: 1, activation: 'relu'}));
 
 	model.add(tf.layers.batchNormalization());
-	model.add(tf.layers.conv2d({filters: 128, kernelSize: 3, strides: 1, activation: 'relu' }));
+	model.add(tf.layers.conv2d({filters: 256, kernelSize: 3, strides: 1, activation: 'relu' }));
 	model.add(tf.layers.flatten());
 	model.add(tf.layers.dense({units: 100, activation: 'relu'}));
 	model.add(tf.layers.dropout({rate: 0.25}));
