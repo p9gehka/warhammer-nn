6bd6e35fe209a255c5cc9aa19340504f21e2c0c7
p9gehka <p9gehka@mail.ru>
2024-05-12 20:48:56 +0300

    Correct orders

--- static/environment/orders.js
+++ static/environment/orders.js
@@ -15,15 +15,15 @@ export class Orders {
 		}
 		this.orders = {
 			[Action.NextPhase]: [],
+			nextPhaseIndexes: [],
 			[Action.Move]: [],
 			moveIndexes: [],
-			separate_move_nextPhase: [],
 			all: [],
 		};
 
 		this.orders[Action.NextPhase] = [{ action: Action.NextPhase }];
 		this.orders[Action.NextPhase].forEach((order) => {
-			this.orders.c.push(this.orders.all.length);
+			this.orders.nextPhaseIndexes.push(this.orders.all.length);
 			this.orders.all.push(order);
 		});
 
