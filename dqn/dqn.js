4f16586bfb9d3487ee47ba32c8a15b1b836051d4
p9gehka <p9gehka@mail.ru>
2024-05-16 11:35:52 +0300

    Changee dqn

--- dqn/dqn.js
+++ dqn/dqn.js
@@ -31,11 +31,11 @@ export function createDeepQNetwork(numActions, h, w, c) {
 	}
 	const inputShape = [h, w, c];
 	const model = tf.sequential();
-	model.add(tf.layers.conv2d({filters: 64, kernelSize: 8, activation: 'relu', inputShape }));
+	model.add(tf.layers.conv2d({filters: 128, kernelSize: 8, activation: 'relu', inputShape }));
 	model.add(tf.layers.batchNormalization());
-	model.add(tf.layers.conv2d({filters: 128, kernelSize: 4, activation: 'relu'}));
+	model.add(tf.layers.conv2d({filters: 256, kernelSize: 4, activation: 'relu'}));
 	model.add(tf.layers.batchNormalization());
-	model.add(tf.layers.conv2d({filters: 128, kernelSize: 3, activation: 'relu' }));
+	model.add(tf.layers.conv2d({filters: 256, kernelSize: 3, activation: 'relu' }));
 	model.add(tf.layers.flatten());
 	model.add(tf.layers.dense({units: 100, activation: 'relu'}));
 	model.add(tf.layers.dropout({rate: 0.25}));
