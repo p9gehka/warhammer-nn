409a1b27bdac18378cb4bde335055f15802309b2
p9gehka <p9gehka@mail.ru>
2024-05-19 01:06:05 +0300

    Reduce conv2 3 kernelSize

--- dqn/dqn.js
+++ dqn/dqn.js
@@ -34,6 +34,8 @@ export function createDeepQNetwork(numActions, h, w, c) {
 	model.add(tf.layers.conv2d({filters: 1, kernelSize: 8, activation: 'relu', inputShape }));
 	model.add(tf.layers.batchNormalization());
 	model.add(tf.layers.conv2d({filters: 1, kernelSize: 4, activation: 'relu'}));
+	model.add(tf.layers.batchNormalization());
+	model.add(tf.layers.conv2d({filters: 1, kernelSize: 2, activation: 'relu' }));
 	model.add(tf.layers.flatten());
 	model.add(tf.layers.dense({units: 100, activation: 'relu'}));
 	model.add(tf.layers.dropout({rate: 0.25}));
