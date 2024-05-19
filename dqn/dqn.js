33cc424f5086fb41d7c9c7ab77f169fd07e0a2f3
p9gehka <p9gehka@mail.ru>
2024-05-19 21:30:52 +0300

    Fix kernel size

--- dqn/dqn.js
+++ dqn/dqn.js
@@ -33,9 +33,9 @@ export function createDeepQNetwork(numActions, h, w, c) {
 	const model = tf.sequential();
 	model.add(tf.layers.conv2d({filters: 1, kernelSize: 4, activation: 'relu', inputShape }));
 	model.add(tf.layers.batchNormalization());
-	model.add(tf.layers.conv2d({filters: 1, kernelSize: 2, activation: 'relu'}));
+	model.add(tf.layers.conv2d({filters: 1, kernelSize: 1, activation: 'relu'}));
 	model.add(tf.layers.batchNormalization());
-	model.add(tf.layers.conv2d({filters: 1, kernelSize: 0, activation: 'relu' }));
+	model.add(tf.layers.conv2d({filters: 1, kernelSize: 1, activation: 'relu' }));
 	model.add(tf.layers.flatten());
 	model.add(tf.layers.dense({units: 100, activation: 'relu'}));
 	model.add(tf.layers.dropout({rate: 0.25}));
