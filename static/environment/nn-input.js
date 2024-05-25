c3318f1125bc3d982be45999923c513a7d4cfab1
p9gehka <p9gehka@mail.ru>
2024-05-25 13:13:36 +0300

    CHange nn-input stamina values

--- static/environment/nn-input.js
+++ static/environment/nn-input.js
@@ -4,7 +4,7 @@ import { len } from '../utils/vec2.js';
 //{ Empty: 0 }
 export const Channel0 = { 0: 1 };
 export const Channel1 = { };
-[0,1,2,3,4,5,6,7,8,9,10].forEach(v => { Channel1[`Stamina${v}`] = v/10; });
+[0,1,2,3,4,5,6,7,8,9,10].forEach(v => { Channel1[`Stamina${v}`] = v; });
 
 export const Channel2 = { ObjectiveMarker: 1 };
 
