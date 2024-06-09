ee881bf30cd89fac711678fae5f11589a5ff1ac2
p9gehka <p9gehka@mail.ru>
2024-06-09 03:08:38 +0300

    36x30

--- static/battlefield/36x30.js
+++ static/battlefield/36x30.js
@@ -2,36 +2,36 @@ import { DeploymentCommon } from './deployment-common.js';
 
 export class Medium36_1 extends DeploymentCommon {
 	deploy_markers = [];
-	nomansland_markers = [[9, 15], [19, 9], [29, 15], [19, 21]];
+	nomansland_markers = [[8, 15], [18, 9], [28, 15], [18, 21]];
 	objective_markers = [...this.deploy_markers, ...this.nomansland_markers];
 }
 
 export class Medium36_2 extends DeploymentCommon {
 	deploy_markers = [];
-	nomansland_markers = [[4, 23], [11, 7], [19, 15], [27, 23], [34, 7]];
+	nomansland_markers = [[3, 23], [10, 7], [18,15], [26, 23], [33, 7]];
 	objective_markers = [...this.deploy_markers, ...this.nomansland_markers];
 }
 
 export class Medium36_3 extends DeploymentCommon {
 	deploy_markers = [];
-	nomansland_markers = [[4, 15], [19, 7], [34, 15], [19, 23]]
+	nomansland_markers = [[3, 15], [18, 7], [33, 15], [18, 23]]
 	objective_markers = [...this.deploy_markers, ...this.nomansland_markers];
 }
 
 export class Medium36_4 extends DeploymentCommon {
 	deploy_markers = [];
-	nomansland_markers = [[9, 11], [19, 9], [29, 19], [19, 21]];
+	nomansland_markers = [[8, 11], [18, 9], [28, 19], [18, 21]];
 	objective_markers = [...this.deploy_markers, ...this.nomansland_markers];
 }
 
 export class Medium36_5 extends DeploymentCommon {
 	deploy_markers = [];
-	nomansland_markers = [[4, 21], [16, 6],  [34, 9],  [22, 24]];
+	nomansland_markers = [[3, 21], [15, 6],  [33, 9],  [21, 24]];
 	objective_markers = [...this.deploy_markers, ...this.nomansland_markers];
 }
 
 export class Medium36_6 extends DeploymentCommon {
 	deploy_markers = [];
-	nomansland_markers = [[5, 15], [19, 7], [33, 15], [19, 23]];
+	nomansland_markers = [[4, 15], [18, 7], [32, 15], [18, 23]];
 	objective_markers = [...this.deploy_markers, ...this.nomansland_markers];
 }
