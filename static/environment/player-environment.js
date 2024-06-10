d898ae5b36412d23e58d7ec6058093e743da14da
p9gehka <p9gehka@mail.ru>
2024-06-10 11:57:24 +0300

    22x22

--- static/environment/player-environment.js
+++ static/environment/player-environment.js
@@ -5,8 +5,8 @@ import { getStateTensor } from '../utils/get-state-tensor.js';
 import { eq } from '../utils/vec2.js';
 
 export class PlayerEnvironment {
-	width = 30;
-	height = 30;
+	width = 22;
+	height = 22;
 	channels = channels;
 	vp = 0;
 	_selectedModel = null;
