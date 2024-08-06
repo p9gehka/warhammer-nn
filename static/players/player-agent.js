7dadb7d699d6f33a1fa1f5797543bac869140d86
p9gehka <p9gehka@mail.ru>
2024-08-06 23:11:19 +0300

    Use moveagent60x44

--- static/players/player-agent.js
+++ static/players/player-agent.js
@@ -1,5 +1,5 @@
 import { Action } from '../environment/orders.js';
-import { MoveAgent } from '../agents/move-agent/move-agent44x30.js';
+import { MoveAgent } from '../agents/move-agent/move-agent60x44.js';
 
 export class PlayerAgent {
 	static cascad = [MoveAgent.settings]
