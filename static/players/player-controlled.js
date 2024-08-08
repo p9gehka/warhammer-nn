872584c9f17d92436b451e280a1f609455011219
p9gehka <p9gehka@mail.ru>
2024-08-09 00:42:48 +0300

    Correct controlled player

--- static/players/player-controlled.js
+++ static/players/player-controlled.js
@@ -7,7 +7,7 @@ export class PlayerControlled {
 		this.env = env;
 		this.playerId = playerId;
 		this.opponentId = (playerId+1) % 2;
-		this._selectedModel = this.env.players[this.playerId].models[0];
+		this._selectedModel = 0;
 	}
 	async playStep() {
 		const orders = await this.orderPromise;
@@ -21,19 +21,19 @@ export class PlayerControlled {
 		const prevState = this.env.getState();
 
 		if (action === PlayerAction.Select) {
-			this._selectedModel = this.env.players[this.playerId].models[order.id];
-			playerOrder = { ...order, id: this._selectedModel };
+			this._selectedModel = order.id;
+			playerOrder = { ...order, id: this._getPlayerSelectedModel() };
 		} else if (action === PlayerAction.SetTarget && this._selectedModel !== null) {
 			const selectedWeapon = this._shootingQueue.at(-1);
 			if (this._shootingTargeting[selectedWeapon] === undefined) {
 				this._shootingTargeting[selectedWeapon] = {};
 			}
-			if (this._shootingTargeting[selectedWeapon][this._selectedModel] === undefined) {
-				this._shootingTargeting[selectedWeapon][this._selectedModel] = [];
+			if (this._shootingTargeting[selectedWeapon][this._getPlayerSelectedModel()] === undefined) {
+				this._shootingTargeting[selectedWeapon][this._getPlayerSelectedModel()] = [];
 			}
-			this._shootingTargeting[selectedWeapon][this._selectedModel].push(this.env.players[this.opponentId].units[order.id].id);
-			if (this._shootingTargeting[selectedWeapon][this._selectedModel].length > this.env.gameSettings.rangedWeapons[this._selectedModel].filter(v=> v.name === selectedWeapon).length) {
-				this._shootingTargeting[selectedWeapon][this._selectedModel].shift();
+			this._shootingTargeting[selectedWeapon][this._getPlayerSelectedModel()].push(this.env.players[this.opponentId].units[order.id].id);
+			if (this._shootingTargeting[selectedWeapon][this._getPlayerSelectedModel()].length > this.env.gameSettings.rangedWeapons[this._getPlayerSelectedModel()].filter(v=> v.name === selectedWeapon).length) {
+				this._shootingTargeting[selectedWeapon][this._getPlayerSelectedModel()].shift();
 			}
 			playerOrder = order;
 		} else if (action === PlayerAction.Shoot && this._shootingQueue.length > 0) {
@@ -64,9 +64,9 @@ export class PlayerControlled {
 				target,
 			};
 		} else if (action === PlayerAction.Move) {
-			playerOrder = { action, id: this._selectedModel, vector: order.vector, expense: order.expense };
-		} else if (action === PlayerAction.SelectWeapon && this._selectedModel !== null) {
-			const weaponName = this.env.gameSettings.rangedWeapons[this._selectedModel][order.id].name;
+			playerOrder = { action, id: this._getPlayerSelectedModel(), vector: order.vector, expense: order.expense };
+		} else if (action === PlayerAction.SelectWeapon &&this._getPlayerSelectedModel() !== null) {
+			const weaponName = this.env.gameSettings.rangedWeapons[this._getPlayerSelectedModel()][order.id].name;
 			this._shootingQueue = this._shootingQueue.filter(v => v !== weaponName);
 			if (weaponName !== undefined) {
 				this._shootingQueue.push(weaponName);
@@ -74,8 +74,8 @@ export class PlayerControlled {
 				if (this._shootingTargeting[weaponName] === undefined) {
 					this._shootingTargeting[weaponName] = {};
 				}
-				if (this._shootingTargeting[weaponName][this._selectedModel]) {
-					this._shootingTargeting[weaponName][this._selectedModel] = [];
+				if (this._shootingTargeting[weaponName][this._getPlayerSelectedModel()]) {
+					this._shootingTargeting[weaponName][this._getPlayerSelectedModel()] = [];
 				}
 			}
 
@@ -88,7 +88,9 @@ export class PlayerControlled {
 		const state = this.env.step(playerOrder);
 		return [{ ...playerOrder, misc: state.misc }, state];
 	}
-
+	_getPlayerSelectedModel() {
+		return this.env.players[this.playerId].models[this._selectedModel];
+	}
 	getState() {
 		return { selected: this._selectedModel, shootingQueue: this._shootingQueue, shootingTargeting: this._shootingTargeting };
 	}
