7a955c1a7290dea624707248edec8a3d1a1b2816
p9gehka <p9gehka@mail.ru>
2024-09-12 03:37:26 +0300

    Add hyperparameter-search

---
+++ hyperparameter-search.js
@@ -0,0 +1,59 @@
+import { getTF } from './static/utils/get-tf.js';
+import { trainModelUsingFitDataset, getDataset } from './supervised/supervised-train.js';
+import { MoveAgent } from './static/agents/move-agent/move-center-agent.js';
+import { createDeepQNetwork } from './dqn/dqn.js';
+import * as hpjs from 'hyperparameters';
+
+const tf = await getTF();
+const optimizers = {
+  sgd: tf.train.sgd,
+  adagrad: tf.train.adagrad,
+  adam: tf.train.adam,
+  adamax: tf.train.adamax,
+  rmsprop: tf.train.rmsprop,
+}
+
+const batchSize = 25;
+const optFunction = async ({ learningRate, optimizer }, { dataset }) => {
+	const model = createDeepQNetwork(MoveAgent.settings.orders.length, MoveAgent.settings.width, MoveAgent.settings.height, MoveAgent.settings.channels.length)
+	model.add(tf.layers.softmax());
+	const opimizer = optimizers[optimizer](learningRate);
+	model.compile({
+		optimizer: opimizer,
+		loss: 'categoricalCrossentropy',
+		metrics: ['accuracy'],
+	});
+
+	// train model using defined data
+
+	const h = await trainModelUsingFitDataset(model, dataset);
+
+	//printint out each optimizer and its loss
+	console.log(optimizer);
+	console.log('learning rate: ', learningRate, 'loss: ', h.history.loss[h.history.loss.length - 1]);
+	return { loss: h.history.loss[h.history.loss.length - 1], status: hpjs.STATUS_OK } ;
+};
+
+const hyperTFJS = async () => {
+	const dataset = getDataset();
+	// defining a search space we want to optimize. Using hpjs parameters here
+	const space = {
+		learningRate: hpjs.uniform(0.000001, 0.1),
+		optimizer: hpjs.choice(['sgd', 'adagrad', 'adam', 'adamax', 'rmsprop']),
+	};
+
+	// finding the optimal hyperparameters using hpjs.fmin. Here, 6 is the # of times the optimization function will be called (this can be changed)
+	const trials = await hpjs.fmin(
+		optFunction, space, hpjs.search.randomSearch, 30,
+		{ rng: new hpjs.RandomState(654321), dataset }
+	);
+
+	const opt = trials.argmin;
+
+	//printing out data
+	console.log('trials', trials);
+	console.log('best optimizer:', opt.optimizer);
+	console.log('best learning rate:', opt.learningRate);
+}
+
+hyperTFJS();
--- supervised-train.js
+++ supervised-train.js
@@ -1,116 +1,9 @@
 import * as fs from 'fs';
-import shelljs from 'shelljs';
 import { getTF } from './static/utils/get-tf.js';
-import { Warhammer } from './static/environment/warhammer.js';
-import { MoveAgent } from './static/agents/move-agent/move-center-agent.js';
-import { getStateTensor } from './static/utils/get-state-tensor.js';
-import { createDeepQNetwork } from './dqn/dqn.js';
-import { getRandomInteger } from './static/utils/index.js';
-import { sendDataToTelegram, sendMessage, memoryUsage } from './visualization/utils.js';
-
-import gameSettings from './static/settings/game-settings.json' assert { type: 'json' };
-import allBattlefields from './static/settings/battlefields.json' assert { type: 'json' };
-
-import config from './config.json' assert { type: 'json' };
-const savePath = './models/supervised-dqn/';
-
-let battlefields = config.battlefields.length > 0 ? filterObjByKeys(allBattlefields, config.battlefields) : allBattlefields;
+import { train } from './supervised/supervised-train.js';
 
 const tf = await getTF();
-const batchSize = 1;
-const epochs = 20;
-async function train(nn) {
-	const env = new Warhammer({ gameSettings, battlefields });
-	const agent = new MoveAgent();
-	function getStateAndAnswer() {
-		const state = env.getState();
-		const { orderIndex, order } = agent.playStep(state);		
-		const selected = env.players[state.player].models[0];
-		env.step({ ...order, id: selected });
-		env.reset();
-		env.models[selected].stamina = getRandomInteger(0, 8);
-		return [agent.getInput(state), agent.playStep(state).orderIndex];
-	}
-	function* getStateAndAnswerGeneratorFn() {
-		while(true) {
-			yield getStateAndAnswer();
-		}
-	}
-
-	const myGeneratorDataset = tf.data.generator(getStateAndAnswerGeneratorFn);
-	const dataset = myGeneratorDataset.map(gameToFeaturesAndLabel)
-		.batch(batchSize);
-	
-	const countOrders = new Array(agent.orders.length).fill(0);
-	/*
-	console.log(agent.orders)
-	await myGeneratorDataset.take(10000).forEachAsync(e => countOrders[e[1]]++);
-	console.log(countOrders)
-	*/
-	const model = createDeepQNetwork(agent.orders.length, agent.width, agent.height, agent.channels.length)
-	model.add(tf.layers.softmax());
-	const opimizer = tf.train.adam(config.learningRate)
-	model.compile({
-		optimizer: opimizer,
-		loss: 'categoricalCrossentropy',
-		metrics: ['accuracy'],
-	});
-	model.summary();
-	await trainModelUsingFitDataset(model, dataset);
-
-	function gameToFeaturesAndLabel(record) {
-		return tf.tidy(() => {
-			const [input, orderIndex] = record;
-			const features = getStateTensor([input], agent.height, agent.width, agent.channels).squeeze();
-			const label = tf.oneHot([orderIndex], agent.orders.length);
-			return {xs: features, ys: label};
-		});
-	}
-	process.exit(0);
-	return;
-}
-
-async function trainModelUsingFitDataset(model, dataset) {
-	const trainLogs = [];
-	const beginMs = performance.now();
-	const lossLogs = [];
-	const accuracyLogs = [];
-
-	const fitDatasetArgs = {
-		batchesPerEpoch: batchSize,
-		epochs: epochs,
-		validationData: dataset,
-		validationBatches: 10,
-		callbacks: {
-			onEpochEnd: async (epoch, logs) => {
-				lossLogs.push({ epoch, val_loss: logs.val_loss });
-				accuracyLogs.push({ epoch, val_acc: logs.val_acc });
-				const secPerEpoch =(performance.now() - beginMs) / (1000 * (epoch + 1));
-				if (savePath != null) {
-					if (!fs.existsSync(savePath)) {
-						shelljs.mkdir('-p', savePath);
-					}
-					await model.save(`file://${savePath}`);
-					console.log(`Saved DQN to ${savePath}`);
-				}
-			},
-		}
-	};
-	await model.fitDataset(dataset, fitDatasetArgs);
-
-	await sendConfigMessage(model);
-	await sendDataToTelegram(lossLogs);
-	await sendDataToTelegram(accuracyLogs);
-}
-async function sendConfigMessage(model) {
-	await sendMessage(
-		model.layers.map( layer => `${layer.name.split('_')[0]}{${ ['filters', 'kernelSize', 'units', 'rate'].map(filter => layer[filter] ? `filter: ${layer[filter]}` : '').filter(v=>v !=='') }}` ).join('->')
-	);
-	await sendMessage(
-		`Supervised hyperparams Config batchSize:${batchSize} learningRate:${config.learningRate}`
-	);
-}
-
+const savePath = '../models/supervised-dqn/';
 async function main() {
 	let nn;
 	if (fs.existsSync(`${savePath}/loading/model.json`)) {
@@ -122,6 +15,8 @@ async function main() {
 		}
 	}
 	await train(nn);
+	process.exit(0);
+	return;
 }
 
 main();
