328f1e215c2f17bd1d67de3adbadbb53de2a8938
p9gehka <p9gehka@mail.ru>
2024-09-13 01:46:13 +0300

    Increase mnist model

--- test-mnist-nn/model.js
+++ test-mnist-nn/model.js
@@ -21,29 +21,30 @@ const tf = await getTF();
 export const model = tf.sequential();
 model.add(tf.layers.conv2d({
   inputShape: [44, 30, 3],
-  filters: 8,
-  kernelSize: [8, 6],
+  filters: 32,
+  kernelSize: 3,
   activation: 'relu',
 }));
-  model.add(tf.layers.batchNormalization());
 model.add(tf.layers.conv2d({
   filters: 32,
-  kernelSize: [4,3],
+  kernelSize: 3,
   activation: 'relu',
 }));
-model.add(tf.layers.batchNormalization());
 model.add(tf.layers.conv2d({
   filters: 32,
-  kernelSize: [4,3],
+  kernelSize: 3,
   activation: 'relu',
 }));
-model.add(tf.layers.batchNormalization());
 model.add(tf.layers.conv2d({
-   filters: 32,
-  kernelSize: [4,3],
+  filters: 64,
+  kernelSize: 3,
   activation: 'relu',
 }));
-model.add(tf.layers.batchNormalization());
+model.add(tf.layers.conv2d({
+  filters: 64,
+  kernelSize: 3,
+  activation: 'relu',
+}));
 model.add(tf.layers.flatten());
 model.add(tf.layers.dropout({rate: 0.25}));
 model.add(tf.layers.dense({units: 2000, activation: 'relu'}));
