4dc65b438e94a674a8957cfafdfaf7cc877c47e2
p9gehka <p9gehka@mail.ru>
2024-09-27 03:22:43 +0300

    CHange dropout rate

--- dqn/dqn.js
+++ dqn/dqn.js
@@ -38,7 +38,7 @@ export function createDeepQNetwork(numActions, h, w, c) {
 	model.add(tf.layers.conv2d({filters: 32, kernelSize: [4,3], activation: 'relu' }));
 	model.add(tf.layers.flatten());
 	model.add(tf.layers.dense({units: 512, activation: 'relu'}));
-	model.add(tf.layers.dropout({rate: 0.4}));
+	model.add(tf.layers.dropout({rate: 0.25}));
 	model.add(tf.layers.dense({units: numActions}));
 	return model;
 }
