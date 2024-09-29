543e5f68267d4756bc763f08cdf9e1e76f19c8a4
p9gehka <p9gehka@mail.ru>
2024-09-29 19:15:48 +0300

    Change dqn

--- dqn/dqn.js
+++ dqn/dqn.js
@@ -33,11 +33,11 @@ export function createDeepQNetwork(numActions, h, w, c, hyperParams = defaultHyp
 	}
 	const inputShape = [h, w, c];
 	const model = tf.sequential();
-	model.add(tf.layers.conv2d({filters: 8, kernelSize: [6,4], activation: 'relu', inputShape, ...hyperParams[0] }));
+	model.add(tf.layers.conv2d({filters: 4, kernelSize: [6,4], activation: 'relu', inputShape, ...hyperParams[0] }));
 	model.add(tf.layers.batchNormalization());
-	model.add(tf.layers.conv2d({filters: 16, kernelSize: [4,3], activation: 'relu', ...hyperParams[1] }));
+	model.add(tf.layers.conv2d({filters: 8, kernelSize: [4,3], activation: 'relu', ...hyperParams[1] }));
 	model.add(tf.layers.batchNormalization());
-	model.add(tf.layers.conv2d({filters: 16, kernelSize: [4,3], activation: 'relu', ...hyperParams[2] }));
+	model.add(tf.layers.conv2d({filters: 12, kernelSize: [4,3], activation: 'relu', ...hyperParams[2] }));
 	model.add(tf.layers.flatten());
 	model.add(tf.layers.dense({units: 512, activation: 'relu', ...hyperParams[3] }));
 	model.add(tf.layers.dropout({rate: 0.4}));
