c6273a31a4baee9b8d5170b5d52ba614a5d9b158
p9gehka <p9gehka@mail.ru>
2024-10-07 23:23:06 +0300

    Player-easy, at test reward

--- tests/test-reward.js
+++ tests/test-reward.js
@@ -2,6 +2,7 @@ import { Warhammer } from '../static/environment/warhammer.js';
 import { PlayerDumb } from '../static/players/player-dumb.js';
 import { filterObjByKeys } from '../static/utils/index.js';
 import { PlayerAgent } from '../static/players/player-agent.js';
+import { PlayerEasy } from '../static/players/player-easy.js';
 import { Rewarder } from '../students/student.js';
 import { MovingAverager } from '../moving-averager.js';
 
@@ -15,7 +16,7 @@ const rewardAveragerLen = 200;
 
 export async function testReward(silent, nn) {
 	const env = new Warhammer({ gameSettings, battlefields });
-	const players = [new PlayerAgent(0, env), new PlayerDumb(env)];
+	const players = [new PlayerEasy(0, env), new PlayerDumb(env)];
 	players[0].agent.onlineNetwork = nn;
 	const rewarder = new Rewarder(env, players[0]);
 
