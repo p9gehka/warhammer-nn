61de0d32d967a394404554976a76fda721df4872
p9gehka <p9gehka@mail.ru>
2024-11-05 22:42:37 +0300

    Rotate map for player2

--- static/agents/move-agent/move-agent.js
+++ static/agents/move-agent/move-agent.js
@@ -5,6 +5,27 @@ import { getInput } from '../../environment/nn-input.js';
 import { eq } from '../../utils/vec2.js';
 import { RandomAgent } from '../random-agent.js';
 
+function channelRevers(channelName, totalObjects) {
+	const convertObjective = totalObjects === 5 ? { ObjectiveMarker5: 'ObjectiveMarker1', ObjectiveMarker1: 'ObjectiveMarker5', ObjectiveMarker4: 'ObjectiveMarker2',  ObjectiveMarker2: 'ObjectiveMarker4'}
+		: { ObjectiveMarker1: 'ObjectiveMarker3', ObjectiveMarker2: 'ObjectiveMarker4', ObjectiveMarker3: 'ObjectiveMarker1', ObjectiveMarker4: 'ObjectiveMarker2'};
+
+		if (convertObjective[channelName] !== undefined) {
+			return convertObjective[channelName];
+		}
+	return channelName;
+}
+function rotateInput(input, channels) {
+	const result = {};
+	let totalObjects = input.ObjectiveMarker5.length === 0 ? 4 : 5;
+	channels.forEach((channel, i) => {
+			for (let entity in channel) {
+				result[channelRevers(entity, totalObjects)] = input[entity].map(pos => [44 - pos[0], 30 - pos[1]])
+			}
+	});
+	//const input = state.player === 0 ? argInput : rotateInput(argInput);
+	return result;
+}
+
 const tf = await getTF();
 
 export class MoveAgentBase {
@@ -18,12 +39,17 @@ export class MoveAgentBase {
 			return this.fillAgent.playStep(state);
 		}
 		const { orders, height, width, channels } = this;
-		const input = getInput(state, playerState);
+
+		let input = getInput(state, playerState);
+		const originalInput = input;
+		if (this.orderRevers !== undefined && state.player === 1) {
+			input = rotateInput(input, channels)
+		}
 
 		let orderIndex = 0;
 		let estimate = 0;
 		const selected = state.models[state.players[state.player].models[playerState.selected]];
-		if (input[Channel1Name.Stamina0].some(pos => eq(pos, selected)))  {
+		if (originalInput[Channel1Name.Stamina0].some(pos => eq(pos, selected)))  {
 			orderIndex = 0;
 		} else {
 			tf.tidy(() => {
@@ -31,6 +57,9 @@ export class MoveAgentBase {
 				const prediction = this.onlineNetwork.predict(inputTensor);
 				estimate = prediction.max(-1).dataSync()[0];
 				orderIndex = prediction.argMax(-1).dataSync()[0];
+				if (this.orderRevers !== undefined && state.player === 1) {
+					orderIndex = this.orderRevers[orderIndex];
+				}
 			});
 		}
 
--- static/agents/move-agent/move-agent44x30.js
+++ static/agents/move-agent/move-agent44x30.js
@@ -10,26 +10,31 @@ const distancesDiagonal = [1, 2, 4];
 const distancesDiagonalExpense = [2, 3, 6];
 const angles = [0, 90, 180, 270];
 
-export const moveOrders = [{ action: BaseAction.NextPhase }];
+const orderRevers = [0, 5,6,7,8,1,2,3,4];
+
 const angleSymbol = {'0': '↓' , '90': '←', '180': '↑' , '270': '→', '0+90': '↙', '90+180': '↖', '180+270': '↗', '270+0': '↘' };
-angles.forEach((angle, i) => {
-	for (let distance of distances) {
-		moveOrders.push({ action: BaseAction.Move, vector: round(angleToVec2(distance, angle)), expense: distance, symbol: angleSymbol[angle] });
-	}
-	/*
-	distancesDiagonal.forEach((distance, ii) => {
-		const vector1 = angleToVec2(distance, angle);
-		const vector2 = angleToVec2(distance, angles[(i+1) % angles.length]);
-		const vector = round(add(vector1, vector2));
-		moveOrders.push({ action: BaseAction.Move, vector, expense: distancesDiagonalExpense[ii] });
-	});
-	*/
-});
+
+export const moveOrders = [
+	{ action: BaseAction.NextPhase },
+	{ action: BaseAction.Move, vector: round(angleToVec2(1, 0)), expense: 1, symbol: '↓' },
+	{ action: BaseAction.Move, vector: round(angleToVec2(6, 0)), expense: 6, symbol: '↓' },
+	{ action: BaseAction.Move, vector: round(angleToVec2(1, 90)), expense: 1, symbol: '←' },
+	{ action: BaseAction.Move, vector: round(angleToVec2(6, 90)), expense: 6, symbol: '←' },
+
+	{ action: BaseAction.Move, vector: round(angleToVec2(1, 180)), expense: 1, symbol: '↑' },
+	{ action: BaseAction.Move, vector: round(angleToVec2(6, 180)), expense: 6, symbol: '↑' },
+	{ action: BaseAction.Move, vector: round(angleToVec2(1, 270)), expense: 1, symbol: '→' },
+
+	{ action: BaseAction.Move, vector: round(angleToVec2(6, 270)), expense: 6, symbol: '→' }
+
+];
+
 export class MoveAgent extends MoveAgentBase {
 	static settings = { width: 44, height: 30, orders: moveOrders, channels: [Channel0, Channel1, Channel2, Channel3, Channel4] }
 	width = MoveAgent.settings.width;
 	height = MoveAgent.settings.height;
 	channels = MoveAgent.settings.channels;
 	orders = MoveAgent.settings.orders;
+	orderRevers = orderRevers;
 	loadPath = `agents/move-agent/.model${this.width}x${this.height}x${this.channels.length}/model.json`;
 }
