4f225a891767799aa68b437bcf6168fdada16dd8
p9gehka <p9gehka@mail.ru>
2024-11-07 12:03:02 +0300

    Correct supervised model

--- supervised/model.js
+++ supervised/model.js
@@ -1,6 +1,6 @@
 import { getTF } from '../static/utils/get-tf.js';
 import { createDeepQNetwork } from '../dqn/dqn.js';
-import { MoveAgent } from '../static/agents/move-agent/move-to-object-agent.js';
+import { MoveAgent } from '../static/agents/move-agent/move-agent60x44.js';
 
 const tf = await getTF();
 export const model = createDeepQNetwork(MoveAgent.settings.orders.length, MoveAgent.settings.height, MoveAgent.settings.width, MoveAgent.settings.channels.length, { addSoftmaxLayer: true });
