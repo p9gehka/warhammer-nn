cb6d92c08b62de8df835687d00038e86b9b68782
p9gehka <p9gehka@mail.ru>
2024-12-14 02:34:53 +0300

    Revert "Unique play agent client memory"
    This reverts commit f1272d5770a2fcb3e59fc4fded240a9e0f5b6a35.

--- tests/prioritized-replay-memory.test.js
+++ tests/prioritized-replay-memory.test.js
@@ -1,12 +1,18 @@
 import { PrioritizedReplayMemory } from '../replay-memory/prioritized-replay-memory.js';
+<<<<<<< HEAD
+=======
+import config from '../config.json' assert { type: 'json' };
+
+const { replayBufferSize } = config;
+>>>>>>> parent of f1272d5 (Unique play agent client memory)
 
 describe('prioritized-replay-memory', () => {
 	const memoryLength = 10;
 	it('append', () => {
 		const memory = new PrioritizedReplayMemory(memoryLength);
-		memory.append('data1', 200)
-		memory.append('data2', 50)
-		memory.append('data3', 50)
+		memory.append('data1', 50)
+		memory.append('data2', 100)
+		memory.append('data3', 150)
 		expect(memory.buffer).toEqual(['data1', 'data2','data3', null, null, null, null, null, null, null]);
 		expect(memory.sumTree.getPriorities()).toEqual([200, 50, 50, 0, 0, 0, 0, 0, 0, 0]);
 
