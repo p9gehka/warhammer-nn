7a9fa1990fecb5476c99636e4def1ee5165d39c2
p9gehka <p9gehka@mail.ru>
2024-12-31 02:02:03 +0300

    Remove last dropout

--- dqn/dqn.js
+++ dqn/dqn.js
@@ -34,7 +34,6 @@ export function createDeepQNetwork(numActions, h, w, c, { addSoftmaxLayer } = { 
 	const inputConv2d = tf.input({shape: inputShape});
 	const inputDense = tf.input({shape: [totalRounds]});
 	let conv2d = tf.layers.conv2d({ filters: 8, kernelSize: 4, activation: 'relu'}).apply(inputConv2d);
-	conv2d = tf.layers.batchNormalization().apply(conv2d);
 	conv2d = tf.layers.conv2d({ filters: 16, kernelSize: 4, activation: 'relu' }).apply(conv2d);
 	conv2d = tf.layers.conv2d({ filters: 16, kernelSize: 4, activation: 'relu' }).apply(conv2d);
 	conv2d = tf.layers.conv2d({ filters: 16, kernelSize: 4, activation: 'relu'}).apply(conv2d);
