20cd69301f6e40e6d2516ad7436cb1afd96ed9b2
p9gehka <p9gehka@mail.ru>
2025-01-19 03:24:33 +0300

    Remove phase check

--- students/student.js
+++ students/student.js
@@ -142,11 +142,8 @@ export class Rewarder {
 	}
 
 	primaryReward(order, primaryVP) {
-		let reward = 0;
-		if (order.action === BaseAction.NextPhase) {
-			reward += (primaryVP - this.primaryVP);
-			this.primaryVP = primaryVP;
-		}
+		const reward = (primaryVP - this.primaryVP);
+		this.primaryVP = primaryVP;
 		return reward;
 	}
 
