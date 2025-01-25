e23c2ae3d9df0034f29849ce476c44f203faaf87
p9gehka <p9gehka@mail.ru>
2025-01-25 12:10:55 +0300

    Change select next Model

--- static/players/player-agent.js
+++ static/players/player-agent.js
@@ -72,16 +72,9 @@ export class PlayerAgent {
 	}
 	selectNextModel(state) {
 		const playerModels = state.players[this.playerId].models;
-		const twicePlayerModels = [...playerModels, ...playerModels];
-		let newSelectedModel = this._selectedModel;
-		for (let i = newSelectedModel + 1; i < twicePlayerModels.length; i++) {
-			let modelId = twicePlayerModels[i];
-			if (!isNaN(state.models[modelId][0])) {
-				newSelectedModel = i % playerModels.length;
-				break;
-			}
-		}
-		return newSelectedModel;
+		const id = playerModels.findIndex((modelId, playerModelId) => playerModelId > this._selectedModel && state.modelsStamina[modelId] > 0);
+
+		return id >= 0 ? id : 0;
 	}
 
 	getState() {
